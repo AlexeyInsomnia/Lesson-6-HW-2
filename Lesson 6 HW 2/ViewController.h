1f2174f6a0272e8e1b8bd0892772204b996cfb4b
AlexeyUr <alisvcom@gmail.com>
2015-12-15 22:03:26 +0300

    Initial Commit

---
+++ Lesson 6 HW 2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lesson 6 HW 2
+//
+//  Created by Alex on 15.12.15.
+//  Copyright © 2015 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
